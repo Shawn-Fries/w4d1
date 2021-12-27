20a149bf538998b29dde71b7bc09ccefb1a3cd5f
Ka Yi Leung <email@email.com87kleung@gmail.comgit config user.email>
2021-12-27 14:10:25 -0800

    slideable

--- chess/chess1/slideable.rb
+++ chess/chess1/slideable.rb
@@ -1,4 +1,21 @@
 module Slideable
     
+    def moves
+        moves = []
+        if move_dirs.include?("horizontal")
+            invalid = false
+            i = 0
+
+            until invalid
+                
+                if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
+                    moves << self[@pos.first + i, @pos.last] 
+                else 
+                    invalid = true
+                end
+                i += 1
+            end
+        end
+    end
     
 end
