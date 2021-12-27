8681ef537225e0be45671c05443d472cb2031c36
Shawn-Fries <samfoley275@gmail.com>
2021-12-27 15:17:23 -0500

    started chess

---
+++ game.rb
@@ -0,0 +1,8 @@
+class Game
+    def initialize(board, display, players, current_player)
+        @board = board
+        @display = display
+        @players = players
+        @current_player = current_player
+    end
+end
