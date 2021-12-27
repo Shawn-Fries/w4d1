aa23a7bcd05e673c381b43a87f5da520762a49ba
Shawn-Fries <samfoley275@gmail.com>
2021-12-27 16:03:28 -0500

    working on moving pieces

--- chess/board.rb
+++ chess/board.rb
@@ -6,7 +6,15 @@ class Board
 
     end
 
-    def move_piece(start_pos, end_pos)
+    def [](pos)
+        row, col = pos
+    end
+
+    def []=(pos, value)
+        pos = [value]
+    end
+
+    def move_piece(color, start_pos, end_pos)
         raise "no piece in that location" if @rows[start_pos.first][start_pos.last] == nil
         raise "invalid move" unless valid_pos?(end_pos)
         @rows[end_pos.first][end_pos.last] = @rows[start_pos.first][start_pos.last]
@@ -14,7 +22,13 @@ class Board
 
     end
 
-    def valid_pos?
-        
+    def valid_pos?(pos)
+        row, col = pos
+        return false if row < 0 or row > 8
+        return false if col < 0 or col > 8
+    end
+
+    def add_piece(piece, pos)
+        pos = [piece]
     end
 end
