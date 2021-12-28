44567a378c3939d7e4e575488e32030814faf7c2
Ka Yi Leung <email@email.com87kleung@gmail.comgit config user.email>
2021-12-28 13:45:53 -0800

    working on slideable and added grow unblocked moves in dir

--- chess/chess1/slideable.rb
+++ chess/chess1/slideable.rb
@@ -14,109 +14,128 @@ DIAGONAL_DIRS = [
     [:dx, :dy]
 ].freeze
 
-def horizontal_dirs
-    horizontal_dirs
-end
+    def horizontal_dirs
+        horizontal_dirs
+    end
 
-def diagonal_dirs
-    diagonal_dirs
-end
+    def diagonal_dirs
+        diagonal_dirs
+    end
 
-def moves
-    moves = []
-    if move_dirs.include?("horizontal")
-        invalid = false
-        i = 0
-        until invalid
-            
-            if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
-                moves << [@pos.first + i, @pos.last] 
-                invalid = true if
-            else 
-                invalid = true
+    def moves
+        moves = []
+        if move_dirs.include?("horizontal")
+            invalid = false
+            i = 0
+            until invalid
+                
+                if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
+                    moves << [@pos.first + i, @pos.last] 
+                    invalid = true if @board[@pos.first + i, @pos.last].color != @color
+                else 
+                    invalid = true
+                end
+                i += 1
             end
-            i += 1
+            invalid = false
+            i = 0
+            until invalid
+                
+                if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
+                    moves << [@pos.first - i, @pos.last] 
+                else 
+                    invalid = true
+                end
+                i += 1
+            end
+            invalid = false
+            i = 0
+            until invalid
+                
+                if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
+                    moves << [@pos.first, @pos.last + i] 
+                else 
+                    invalid = true
+                end
+                i += 1
+            end
+            invalid = false
+            i = 0
+            until invalid
+                
+                if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
+                    moves << [@pos.first, @pos.last - i] 
+                else 
+                    invalid = true
+                end
+                i += 1
+            end
         end
-        invalid = false
-        i = 0
-          until invalid
-            
-            if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
-                moves << [@pos.first - i, @pos.last] 
-            else 
-                invalid = true
+        if move_dirs.include?("diagonal")
+            invalid = false
+            i = 0
+            until invalid
+                
+                if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
+                    moves << [@pos.first + i, @pos.last + i] 
+                else 
+                    invalid = true
+                end
+                i += 1
             end
-            i += 1
-        end
-        invalid = false
-        i = 0
-          until invalid
-            
-            if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
-                moves << [@pos.first, @pos.last + i] 
-            else 
-                invalid = true
+            invalid = false
+            i = 0
+            until invalid
+                
+                if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
+                    moves << [@pos.first - i, @pos.last - i] 
+                else 
+                    invalid = true
+                end
+                i += 1
             end
-            i += 1
-        end
-        invalid = false
-        i = 0
-          until invalid
-            
-            if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
-                moves << [@pos.first, @pos.last - i] 
-            else 
-                invalid = true
+            invalid = false
+            i = 0
+            until invalid
+                
+                if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
+                    moves << [@pos.first + i, @pos.last - i] 
+                else 
+                    invalid = true
+                end
+                i += 1
             end
-            i += 1
+            invalid = false
+            i = 0
+            until invalid
+                
+                if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
+                    moves << [@pos.first - i, @pos.last + i] 
+                else 
+                    invalid = true
+                end
+                i += 1
+            end
         end
     end
-    if move_dirs.include?("diagonal")
+
+
+    def grow_unblocked_moves_in_dir(dx, dy)
         invalid = false
-        i = 0
-        until invalid
-            
-            if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
-                moves << [@pos.first + i, @pos.last + i] 
-            else 
-                invalid = true
+            i = 0
+            until invalid
+                if @board.valid_pos?([@pos.first + dx, @pos.last + dy]) && (@board[@pos.first + dx, @pos.last + dy] == nil || @board[@pos.first + dx, @pos.last + dy].color != @color)
+                    moves << [@pos.first + dx, @pos.last + dy] 
+                    invalid = true if @board[@pos.first + dx, @pos.last + dy].color != @color
+                else 
+                    invalid = true
+                end
+                i += 1
             end
-            i += 1
-        end
-        invalid = false
-        i = 0
-          until invalid
-            
-            if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
-                moves << [@pos.first - i, @pos.last - i] 
-            else 
-                invalid = true
-            end
-            i += 1
-        end
-        invalid = false
-        i = 0
-          until invalid
-            
-            if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
-                moves << [@pos.first + i, @pos.last - i] 
-            else 
-                invalid = true
-            end
-            i += 1
-        end
-        invalid = false
-        i = 0
-          until invalid
-            
-            if @board.valid_pos?(@pos) && (@board[@pos] == nil || @board[@pos].color != @color)
-                moves << [@pos.first - i, @pos.last + i] 
-            else 
-                invalid = true
-            end
-            i += 1
-        end
+
     end
+
+
+
 end
-    
-end
+
